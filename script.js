9c74cc7a4cf268a95f39ce6ea84776fa2f3c1a0c
munna-68 <mahamudmunna68@gmail.com>
2025-12-19 15:12:49 +0600

    making a partically functional game with simple  but working logic

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let gameBord = ["", "", "", "", "", "", "", "", ""]
+let winningIndexes = {
+  row1:  [0, 1, 2],
+  row2:  [3, 4, 5],
+  row3:  [6, 7, 8],
+  col1:  [0, 3, 6],
+  col2:  [1, 4, 7],
+  col3:  [2, 5, 8],
+  diag1: [0, 4, 8],
+  diag2: [2, 4, 6]
+}
+
+
 const gameBordCell = document.querySelectorAll('.cell')
 
 
@@ -19,20 +32,44 @@ gameBordCell.forEach( cell => { cell.addEventListener("click", () => {
   checkCurrentPlayer()
   cell.textContent = currentPlayer;
   clickCounter++
+  let textContent = cell.textContent;
+  let id = cell.id;
+  updateGameBoard(textContent, id);
+  playTic(gameBord)
+  console.log(gameBord)
 })});
 
-
-let gameBord = ["Y", "Y", "Y", "", "", "", "", "", ""]
-let winningIndexes = {
-  row1:  [0, 1, 2],
-  row2:  [3, 4, 5],
-  row3:  [6, 7, 8],
-  col1:  [0, 3, 6],
-  col2:  [1, 4, 7],
-  col3:  [2, 5, 8],
-  diag1: [0, 4, 8],
-  diag2: [2, 4, 6]
+function updateGameBoard(textContent, id) {
+  if(id === "cell-1-1"){
+    gameBord[0] = textContent;
+  }
+  if(id === "cell-1-2"){
+    gameBord[1] = textContent;
+  }
+  if(id === "cell-1-3"){
+    gameBord[2] = textContent;
+  }
+  if(id === "cell-2-1"){
+    gameBord[3] = textContent;
+  }
+  if(id === "cell-2-2"){
+    gameBord[4] = textContent;
+  }
+  if(id === "cell-2-3"){
+    gameBord[5] = textContent;
+  }
+  if(id === "cell-3-1"){
+    gameBord[6] = textContent;
+  }
+  if(id === "cell-3-2"){
+    gameBord[7] = textContent;
+  }
+  if(id === "cell-3-3"){
+    gameBord[8] = textContent;
+  }
 }
+
+
 
 function playTic(arr) {
   if(winningIndexes.row1.every(i => arr[i] === "X") ||
@@ -58,4 +95,3 @@ function playTic(arr) {
   
 } }
 
-playTic(gameBord)
